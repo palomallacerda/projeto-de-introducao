4a90a81fba4e348090a29aa28c08b09513fc2c89
palomallacerda <56915694+palomallacerda@users.noreply.github.com>
2020-01-21 14:08:47 -0300

    Update desafio.py

--- desafio.py
+++ desafio.py
@@ -1,4 +1,3 @@
-
 ## grafico um com a malha ##
 import pandas as pd
 import matplotlib.pyplot as plt 
@@ -7,32 +6,61 @@ import numpy as np
 
 fort = pd.read_csv("fortaleza.csv") 
 
-
 plt.plot(fort.Ano, fort.Fort)
 plt.xlabel('ANO (s)')
 plt.ylabel('Preciptacao')
 plt.title('Precipitação atmosférica em Fortaleza, CE')
 plt.grid(True)
-plt.tight_layout()
 plt.xticks(fort.Ano, fort.Ano)
 
 plt.show()
 
 ## 10 primeiros com malha##
-plt.clean()
-fort1 = fort[0:10]
+# com malha#
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.pyplot as plt 
 
-x = fort1["Ano"]
-y = fort1["Fort"]
+forta = pd.read_csv("fortaleza.csv") 
+fort1 = forta[0:10]
 
-graf = plt.plot(x,y)
-
+plt.plot(fort1.Ano, fort1.Fort)
 plt.xlabel('ANO (s)')
 plt.ylabel('Preciptacao')
-plt.title('10 primeiros dados da preciptacao atmosférica')
+plt.title('Precipitação atmosférica os 10 primeiros')
 plt.grid(True)
-plt.tight_layout()
-plt.xticks(fort.Ano, fort.Ano)
+plt.xticks(fort1.Ano, fort1.Ano, rotation = 'vertical')
+plt.show()
+
+## 10 ultimos com malha##
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+forta = pd.read_csv("fortaleza.csv") 
+fort1 = forta[139:]
+
+plt.plot(fort1.Ano, fort1.Fort)
+plt.xlabel('ANO (s)')
+plt.ylabel('Preciptacao')
+plt.title('Precipitação atmosférica os 10 primeiros')
+plt.grid(True)
+plt.xticks(fort1.Ano, fort1.Ano, rotation = 'vertical')
 plt.show()
 
 
+#gerando box com malha#
+
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+fort1= pd.read_csv("fortaleza.csv") 
+
+plt.boxplot(fort1.Fort)
+plt.xlabel('ANO(s)')
+plt.ylabel('Preciptacao')
+plt.title('Precipitação atmosférica')
+plt.grid(True)
+
+plt.show()

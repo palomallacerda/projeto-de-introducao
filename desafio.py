4943614c606ea62783e99a6e36462c978ae3c4ad
palomallacerda <56915694+palomallacerda@users.noreply.github.com>
2020-01-22 22:13:49 -0500

    Update desafio.py

--- desafio.py
+++ desafio.py
@@ -5,14 +5,22 @@ import numpy as np
 
 plt.style.use('bmh') ## adiciona cores ##
 #print(plt.style.available)
+
 fort = pd.read_csv("fortaleza.csv") 
 
 plt.plot(fort.Ano, fort.Fort)
+
 plt.xlabel('ANO (s)')
+
 plt.ylabel('Preciptacao')
+
 plt.title('Precipitação atmosférica em Fortaleza, CE')
+
 plt.grid(True)
+
 plt.xticks(fort.Ano, fort.Ano)
+
+plt.savefig('grafico1.jpg')
 
 plt.show()
 
@@ -24,14 +32,23 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 
 forta = pd.read_csv("fortaleza.csv") 
+
 fort1 = forta[0:10]
 
 plt.plot(fort1.Ano, fort1.Fort)
+
 plt.xlabel('ANO (s)')
+
 plt.ylabel('Preciptacao')
+
 plt.title('Precipitação atmosférica os 10 primeiros')
+
 plt.grid(True)
+
 plt.xticks(fort1.Ano, fort1.Ano, rotation = 'vertical')
+
+plt.savefig('10primeiros.jpg')
+
 plt.show()
 
 ## 10 ultimos com malha##
@@ -43,11 +60,19 @@ forta = pd.read_csv("fortaleza.csv")
 fort1 = forta[139:]
 
 plt.plot(fort1.Ano, fort1.Fort)
+
 plt.xlabel('ANO (s)')
+
 plt.ylabel('Preciptacao')
+
 plt.title('Precipitação atmosférica os 10 ultimos')
+
 plt.grid(True)
+
 plt.xticks(fort1.Ano, fort1.Ano, rotation = 'vertical')
+
+plt.savefig('10ultimos.jpg')
+
 plt.show()
 
 
@@ -59,9 +84,15 @@ import matplotlib.pyplot as plt
 fort1= pd.read_csv("fortaleza.csv") 
 
 plt.boxplot(fort1.Fort)
+
 plt.xlabel('ANO(s)')
+
 plt.ylabel('Preciptacao')
+
 plt.title('Precipitação atmosférica')
+
 plt.grid(True)
 
-plt.show()
+plt.savefig('box.jpg')
+
+plt.show()  
